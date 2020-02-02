e9cf58ab5064eb63547c7d9c7d5ec973523c4f9a
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-02-02 15:49:12 -0500

    button changes color depending on difficulty

--- number-fun/my-app/src/Main.js
+++ number-fun/my-app/src/Main.js
@@ -8,7 +8,7 @@ function Main() {
     
   
     useEffect(() => {
-       UpdateCompNum(Math.floor(Math.random() * 10) + 1)
+       EasyMode()
        }, []);
 
   
@@ -21,28 +21,38 @@ function Main() {
             document.getElementById("yay").innerHTML = 'Yay!';
             document.getElementById("again").innerHTML ='<div><button onClick={location.reload()}>Play again?</button></div>';
         } else{
-          document.getElementById('yay').innerHTML = `<div><p> ${HumNum} is not the number</p></div>`
+          document.getElementById('yay').innerHTML = `<div><p> ${HumNum} is incorrect</p></div>`
           
           }
       } 
 
-   const EasyMode = (e) => {
+   const EasyMode = () => {
       UpdateCompNum(Math.floor(Math.random() * 10) + 1)
+      document.getElementById("easy").classList.add('MyClass');
+      document.getElementById("normal").classList.remove('MyClass');
+      document.getElementById("hard").classList.remove('MyClass');
    }
 
-   const NormalMode = (e) => {
+   const NormalMode = () => {
       UpdateCompNum(Math.floor(Math.random() * 50) + 1)
+      document.getElementById("normal").classList.add('MyClass');
+      document.getElementById("easy").classList.remove('MyClass');
+      document.getElementById("hard").classList.remove('MyClass');
      
    }
 
-   const HardMode = (e) => {
+   const HardMode = () => {
       UpdateCompNum(Math.floor(Math.random() * 100) + 1)
+      document.getElementById("hard").classList.add('MyClass');
+      document.getElementById("easy").classList.remove('MyClass');
+      document.getElementById("normal").classList.remove('MyClass');
    }
 
 
   
     return (
 <div>
+   
    <h1>Number Fun <span>(with hooks)</span></h1>
    <ul id="gameDesc">
       <li>Built with React</li>
@@ -51,16 +61,19 @@ function Main() {
       <li>Game is defaulted on easy mode</li>
       <li>Try and get your number equal to the computers..</li>
    </ul>
+  
    <ul id="gameModes">
       <li><button id="easy" onClick={EasyMode}>Easy: 1-10</button></li>
       <li><button id="normal" onClick={NormalMode}>Normal: 1-50</button></li>
       <li><button id="hard" onClick={HardMode}>Hard: 1-100</button></li>
    </ul>
+   
    <p><u>Find the number</u></p>
    <form onSubmit={handleSubmit}>
       <input type="text" value={HumNum} name="name" onChange={(e) => UpdateHumNum( e.target.value)}/>
       <button id="button" type="submit">Guess</button>
    </form>
+   
    <br></br>
    <p id="yay"></p>
    <p id="again"></p>
